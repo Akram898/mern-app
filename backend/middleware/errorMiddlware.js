d1684d4ef1fa22503e8337e6b3d7e14f42dfed5f
Akram898 <akram78787@gmail.com>
2022-09-13 20:27:57 +0400

    update

--- backend/middleware/errorMiddlware.js
+++ backend/middleware/errorMiddlware.js
@@ -5,7 +5,7 @@ const errorHanlder = (err, req, res, next) => {
 
   res.json({
     message: err.message,
-    stack: process.env.NODE_ENV === "production" ? null : err.stack, //we want this condition to work in dev mode only -  if in production mode retuen null, else show error stack which show additional informations
+    stack: process.env.NODE_ENV === "production" ? null : err.stack, //we want this condition to work in dev mode only -  if in production mode return null, else show error stack which show additional informations
   });
 };
 
--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -12,6 +12,9 @@ router.route("/").get(getGoals).post(setGoal);
 router.route("/:id").put(updateGoal).delete(deleteGoal);
 
 // router.get("/", getGoals);
+// router.get('/', (req, res) => {
+//   res.status(200).json({message :'Get goals'})
+// })
 
 // router.post("/", setGoal);
 
